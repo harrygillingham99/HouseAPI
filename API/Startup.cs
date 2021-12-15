0377df6f0d7d7e3bdda085cc80027e12f1d2ac33
Harry Gillingham <harrygillingham99@gmail.com>
2021-12-15 17:37:00 +0000

    Update Startup.cs

--- API/Startup.cs
+++ API/Startup.cs
@@ -66,7 +66,12 @@ namespace House.API
                     builder
                         .AllowAnyHeader()
                         .WithMethods("GET", "POST", "PUT", "DELETE")
-                        .WithOrigins("http://localhost", "https://localhost", "https://localhost:44359", "https://localhost:44370", "http://192.168.0.69:420");
+                        .WithOrigins("http://localhost", 
+                                     "https://localhost", 
+                                     "https://localhost:44359", 
+                                     "https://localhost:44370", 
+                                     "http://192.168.0.69:420", 
+                                     "http://192.168.1.69:84");
                 });
             });
 

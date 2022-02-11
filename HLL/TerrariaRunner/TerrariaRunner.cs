2345e1675c506209ac83d07ff3741c9009b9de03
harrygillingham99 <harrygillingham99@gmail.com>
2022-02-11 15:52:49 +0000

    feat(terraria): wired up process start

--- HLL/TerrariaRunner/TerrariaRunner.cs
+++ HLL/TerrariaRunner/TerrariaRunner.cs
@@ -8,15 +8,19 @@ namespace House.HLL.TerrariaRunner
     {
         private readonly Process _process;
         private readonly TerrariaConfig _config;
+        private bool _isRunning;
 
         public TerrariaRunner(IOptions<TerrariaConfig> options)
         {
+            _isRunning = false;
             _process = new Process();
             _config = options.Value;
         }
 
         public bool Start()
         {
+            if (_isRunning) return true;
+
             var startInfo = new ProcessStartInfo("cmd.exe", $"/c {_config.StartCommand}")
             {
                 WorkingDirectory = _config.ServerDirectory,
@@ -35,7 +39,9 @@ namespace House.HLL.TerrariaRunner
                 if (e.Data != null) Log.Information(e.Data);
             };
 
-            return _process.Start();
+            _isRunning = _process.Start();
+
+            return _isRunning;
         }
     }
 

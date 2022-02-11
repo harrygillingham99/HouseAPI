2bf0a4c65a10326d14187516ccbec6327fd972a0
harrygillingham99 <harrygillingham99@gmail.com>
2022-02-11 15:40:25 +0000

    feat(terraria): scaffolded terraria singleton

---
+++ HLL/TerrariaRunner/TerrariaConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace House.HLL.TerrariaRunner
+{
+    public class TerrariaConfig
+    {
+        public string StartCommand { get; set; }
+        public string ServerDirectory { get; set; }
+    }
+}
---
+++ HLL/TerrariaRunner/TerrariaRunner.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Options;
+using Serilog;
+
+namespace House.HLL.TerrariaRunner
+{
+    public class TerrariaRunner : ITerrariaRunner
+    {
+        private readonly Process _process;
+        private readonly TerrariaConfig _config;
+
+        public TerrariaRunner(IOptions<TerrariaConfig> options)
+        {
+            _process = new Process();
+            _config = options.Value;
+        }
+
+        public bool Start()
+        {
+            var startInfo = new ProcessStartInfo("cmd.exe", $"/c {_config.StartCommand}")
+            {
+                WorkingDirectory = _config.ServerDirectory,
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true
+            };
+            _process.StartInfo = startInfo;
+            _process.ErrorDataReceived += (sender, e) =>
+            {
+                if (e.Data != null) Log.Error(e.Data);
+            };
+            _process.OutputDataReceived += (sender, e) =>
+            {
+                if (e.Data != null) Log.Information(e.Data);
+            };
+
+            return _process.Start();
+        }
+    }
+
+    public interface ITerrariaRunner
+    {
+    }
+}
